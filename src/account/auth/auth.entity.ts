6bd1c27f63c47c23bdb7d12c6eab32af4500e41a
Sasha <ivanpolarus2009@gmail.com>
2023-09-03 11:30:17 +0300

    feat: add user entity

--- src/account/auth/auth.entity.ts
+++ src/account/auth/auth.entity.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './types/user.interface';
+import { genSalt, hash, compare } from 'bcryptjs';
 
 @Injectable()
-export class AuthEntity {}
+export class AuthEntity implements IUser {
+	email: string;
+	password: string;
+	name: string;
+
+	constructor(user: IUser) {
+		this.email = user.email;
+		this.password = user.password;
+		this.name = user.name;
+	}
+
+	async setPassword(password: string) {
+		const salt = await genSalt(10);
+		const passwordHash = await hash(password, salt);
+		this.password = passwordHash;
+	}
+
+	async comparePassword(password: string) {
+		const result = await compare(password, this.password);
+		return result;
+	}
+}
