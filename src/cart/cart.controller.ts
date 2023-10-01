66e31ea413512b130de55d67a5fe0d63c435d334
Sasha <ivanpolarus2009@gmail.com>
2023-10-01 08:20:29 +0300

    feat: add db model, repository

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,22 +1,27 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('cart')
 export class CartController {
+	@UseGuards(AuthGuard('jwt'))
 	@Get('')
 	async getCart() {
 		return {};
 	}
 
+	@UseGuards(AuthGuard('jwt'))
 	@Post('product')
 	async addProduct() {
 		return {};
 	}
 
+	@UseGuards(AuthGuard('jwt'))
 	@Delete(':id')
 	async deleteProduct(@Param('id') id: string) {
 		return id;
 	}
 
+	@UseGuards(AuthGuard('jwt'))
 	@Put(':id')
 	async editQuantity(@Param('id') id: string) {
 		return id;
--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { CartController } from './cart.controller';
+import { CartModel } from './models/cart.model';
+import { CartRepository } from './models/cart.repository';
 
 @Module({
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: CartModel,
+				schemaOptions: {
+					collection: 'Cart',
+				},
+			},
+		]),
+	],
 	controllers: [CartController],
+	providers: [CartRepository],
 })
 export class CartModule {}
